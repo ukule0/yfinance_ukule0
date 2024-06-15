e55d02cd3cc4bbef31988c0de3806a84f29ac4c1
ukule0@gmail.com <ukule0@gmail.com>
2024-06-15 14:20:14 +0900

    initial commit

---
+++ lesson.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+#import requests as req
+
+#ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+#headers = {'User-Agent': ua}
+
+#r=req.get("https://finance.yahoo.com/", headers=headers)
+#r = req.get('https://blog.cosnomi.com', timeout=(60.0, 60.5), headers=headers)
+#print(r.text)
+msft = yf.Ticker("MSFT")
+
+dv = msft.dividends
+
+print(msft.dividends)
+
