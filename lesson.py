3b07bf6e3de5e3e3d4e2a4e34dc68d58b3cd3351
ukule0@gmail.com <ukule0@gmail.com>
2024-06-17 06:03:23 +0900

    CSV からの読み取り機能追加

--- lesson.py
+++ lesson.py
@@ -1,8 +1,30 @@
 import yfinance as yf
+import pandas as pd
 
-msft = yf.Ticker("MSFT")
 
-dv = msft.dividends
+def read_csv():
+    filename = 'nikkei.csv'
+    df = pd.read_csv(filename, encoding='shift-jis')
+    return df
 
-print(msft.dividends)
 
+def main():
+    df_tickers = read_csv()
+    df_tickers['コード'] =  df_tickers['コード'].astype(str) + '.T'
+
+    for ticker in df_tickers['コード']:
+        st_jp = yf.Ticker(ticker)
+        price = st_jp.history('1d')
+        print(price['Close'])
+
+    for it_tuple in df_tickers.itertuples(name=None):
+        str_ticker = it_tuple[1]
+        st_jp = yf.Ticker(str_ticker)
+        price = st_jp.history('1d')['Close'][0,1]
+        it_tuple[3] = price
+        print(it_tuple[1] + " " + str(it_tuple[3]))
+
+
+
+if __name__ == "__main__":
+    main()
