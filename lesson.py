a77178c916b3702418b14fa15f6975585e9f7399
YNishiura <yoichi.nishiura@harman.com>
2024-06-18 17:05:41 +0900

    to_jp_ticker_nameとしてRefactoring

--- lesson.py
+++ lesson.py
@@ -9,20 +9,23 @@ def read_csv():
 
 
 def main():
-    df_tickers = read_csv()
-
-    filter = df_tickers["Market"] != "Tokyo"
-    df2 = df_tickers['コード'].where(filter, df_tickers['コード'].astype(str) + '.T')
-    df_tickers['コード'] = df2
-# ハマリポイント : filterのTrue/Falseは感覚と逆
-# ハマリポイント whereで取得された結果df_tickers['コード']は一旦別の変数df2に格納する
-#    df_tickers['コード'] = df_tickers['コード'].where(df_tickers["Market"] == 'Nasdaq', df_tickers['コード'].astype(str) + '.T')
-#    df_tickers['コード'] = df_tickers['コード'].astype(str) + '.T'
+    df = read_csv()
+    df_tickers = to_jp_ticker_name(df)
 
     for it_tuple in df_tickers.itertuples(name=None):
-        stock = yf.Ticker(it_tuple[1])
-        price = stock.history('7d')
-        print(it_tuple[2] + " " +  str(price.iloc[0].at["Close"]))   #ハマりポイント：DF/Seriesの要素アクセス　行(iloc):連番 , 列(at):列名
+        stock = yf.Ticker(it_tuple[2])
+        price = stock.history('1d')
+        print(it_tuple[3] + " " + str(round(price.iloc[0].at["Close"],2)))  #ハマりポイント：DF/Seriesの要素アクセス　行(iloc):連番 , 列(at):列名
+
+
+def to_jp_ticker_name(df):
+    filter = df["Market"] != "Tokyo"
+    df_temp = df['コード'].where(filter, df['コード'].astype(str) + '.T')
+    df['コード'] = df_temp
+    # ハマリポイント : filterのTrue/Falseは感覚と逆
+    # ハマリポイント df.whereで取得された結果df_tickers['コード']は一旦別の変数df2に格納する
+
+    return df
 
 
 if __name__ == "__main__":
