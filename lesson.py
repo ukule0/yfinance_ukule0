088cbf2c7f9f6abd04a4e9c06ce3713d06a9bdd4
ukule0@gmail.com <ukule0@gmail.com>
2024-06-22 16:43:15 +0900

    関数の構成をリファクタリング

--- lesson.py
+++ lesson.py
@@ -9,13 +9,13 @@ def read_csv():
 
 
 def main():
+    print("USDJPY:" + str(get_close_value("USDJPY=X")))
+
+    'CSVの企業の株価を取得'
     df = read_csv()
     df_tickers = to_jp_ticker_name(df)
-
     for it_tuple in df_tickers.itertuples(name=None):
-        stock = yf.Ticker(it_tuple[2])
-        price = stock.history('1d')
-        print(it_tuple[3] + " " + str(round(price.iloc[0].at["Close"],2)))  #ハマりポイント：DF/Seriesの要素アクセス　行(iloc):連番 , 列(at):列名
+        print(it_tuple[3] + " " + str(get_close_value(it_tuple[2])))
 
 
 def to_jp_ticker_name(df):
@@ -24,15 +24,15 @@ def to_jp_ticker_name(df):
     df['コード'] = df_temp
     # ハマリポイント : filterのTrue/Falseは感覚と逆
     # ハマリポイント df.whereで取得された結果df_tickers['コード']は一旦別の変数df2に格納する
-
     return df
 
-def fx_rates (ticker):
-    ret=  yf.Ticker(ticker).history(period="1d")
-    return ret
 
+def get_close_value(ticker):
+    ret = yf.Ticker(ticker).history(period="1d")
+    ret = ret.iloc[0].at["Close"]     # ハマりポイント：DF/Seriesの要素アクセス　行(iloc):連番 , 列(at):列名
+    ret = round(ret, 2)
+    return ret
 
 
 if __name__ == "__main__":
     main()
-    print(fx_rates("USDJPY=X").iloc[0].at["Close"])
