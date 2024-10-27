8c868e6d26aaf70d49768dc13f4fadb19eaf7fb9
ukule0@gmail.com <ukule0@gmail.com>
2024-10-28 07:15:54 +0900

    関数化[作業中] - ticker取得 - 配当基準日

--- util.py
+++ util.py
@@ -4,24 +4,36 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    ticker = "^N225"
-    ticker = "2267.T" #ヤクルト
-    ticker = "9434.T" #ソフトバンク
-    ret = yf.Ticker(ticker).history(start="1980-01-01")
+    ticker = get_ticker()
+    ret = yf.Ticker(ticker).history(start="2023-01-01")
     stock_data = ret['Close']
     dividends_data = ret['Dividends']
-#    Splits_data = ret['Stock Splits']
+    Splits_data = ret['Stock Splits']
 
     year_start = pd.to_datetime('2023-01-01',utc=True).tz_convert('Asia/Tokyo')
     year_end = pd.to_datetime('2024-01-01',utc=True).tz_convert('Asia/Tokyo')
     year_dividends = dividends_data[(dividends_data.index >= year_start) & (dividends_data.index < year_end)]
-    yearend_stock = stock_data[(stock_data.index >= year_start) & (stock_data.index < year_end)].tail(1).iat[0]
-
+    benchmark_price = get_benchmark_price(stock_data)
+    print("bench_price:" + str(benchmark_price))
     #plt.figure()
-    dividends_per = year_dividends.sum() / yearend_stock * 100
+    dividends_per = year_dividends.sum() / benchmark_price * 100
     print(dividends_per)
     year_dividends.plot()
     plt.show()
+
+
+def get_benchmark_price(stock_data):
+    year_start = pd.to_datetime('2023-03-24T00:00:00',utc=True).tz_convert('Asia/Tokyo')
+    year_end = pd.to_datetime('2023-04-01T00:00:00',utc=True).tz_convert('Asia/Tokyo')
+    yearend_stock = stock_data[(stock_data.index >= year_start) & (stock_data.index < year_end)].tail(1).iat[0]
+    return yearend_stock
+
+
+def get_ticker():
+    #ticker = "^N225"
+    #ticker = "2267.T" #ヤクルト
+    ticker = "9434.T" #ソフトバンク
+    return ticker
 
 
 if __name__ == "__main__":
